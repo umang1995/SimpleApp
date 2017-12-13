71192a7ea6b1ab2c101fe4797828a7bf9ea6a048
ZS\ut15127 <umang_shivam@yahoo.com>
2017-12-13 17:00:52 +0530

    changes done

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello',methods=['GET','POST'])
 def hello():
+    if request.method=="POST":
+        name = request.form['name']
+        email = request.form['email']
+        site = request.form['site_url']
+        comments = request.form['comments']
+        return render_template(
+            'done.html',
+            name=name,
+            email=email,
+            site=site,
+            comments=comments)
     return render_template('hello.html')
-
-
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-    return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
+app.run()
 
